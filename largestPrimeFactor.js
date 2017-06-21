e88f0edabc5843f6f4241dd7d191ccbfce763983
Oksana Harr <oksana.harris@gmail.com>
2017-06-21 10:31:04 -1000

    working but very slowly

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,27 @@
 exports.largestPrimeFactor = function(n){
   var primeNumber = 0;
-  // do your work here
+  var primeArr = [2, 3];
+  var isPrime;
 
+  for (var i = 4; i <= n; i++){
+    isPrime = true;
+    for (var j = 0; j < primeArr.length; j++){
+      if (i % primeArr[j] === 0){
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime === true){
+      primeArr.push(i);
+    }
+  }
+  
+  for (var k = primeArr.length - 1; k >= 0; k--){
+    if (n % primeArr[k] === 0){
+      primeNumber = primeArr[k];
+      break;
+    }
+  }
+  
   return primeNumber;
 };
